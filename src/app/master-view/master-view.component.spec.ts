c6793c54b882aa9c0c7254b63bc468b7dba9582d
Claudio Pi <claudiopi@gmail.com>
2023-02-27 17:35:11 -0300

    ff app updates

--- src/app/master-view/master-view.component.spec.ts
+++ src/app/master-view/master-view.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { IgxButtonModule, IgxRippleModule, IgxButtonModule, IgxRippleModule } from '@infragistics/igniteui-angular';
 import { MasterViewComponent } from './master-view.component';
 
 describe('MasterViewComponent', () => {
@@ -8,10 +11,13 @@ describe('MasterViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MasterViewComponent ]
+      declarations: [ MasterViewComponent ],
+      imports: [ NoopAnimationsModule, FormsModule, IgxButtonModule, IgxRippleModule, IgxButtonModule, IgxRippleModule ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MasterViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
